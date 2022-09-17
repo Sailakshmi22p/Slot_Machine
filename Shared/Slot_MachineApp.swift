9ff5617c70b98e40191249c2f47b1c1d8d618888
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-16 22:13:19 -0400

    Initial Commit

---
+++ Shared/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Shared
+//
+//  Created by Sai Lakshmi on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
